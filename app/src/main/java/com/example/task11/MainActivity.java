dad1e98f4a5a845f0368fc59b67f217119cdaa3d
NoamVak <noamvak765@gmail.com>
2020-10-20 22:59:27 +0300

    need to continue

--- app/src/main/java/com/example/task11/MainActivity.java
+++ app/src/main/java/com/example/task11/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             {"","Germany","United Kingdom","Italy","France","Spain","Netherlands","Sweden"}};
     String [][] citiesA={{"Mombasa","Nairobi","Kisumu","Nakuru","Eldoret"},
             {"Addis Ababa","Mekelle","Gondar","Adama","Hawassa"},
-            {"Accra","Kumsai","Tamale","Ashaiman","Sunyani"},
+            {"Accra","Kumasi","Tamale","Ashaiman","Sunyani"},
             {"Lagos","Kano","Ibadan","Benin City","Jos"},
             {"Casablanca","Fez","Tangier","Marrakesh","Meknes"},
             {"Omdurman","Khartoum","Nyala","Kassala","El-Gadarif"},
@@ -52,6 +52,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             {"Madrid","Barcelona","Seville","Bilbao","Zaragoza"},
             {"Amsterdam","Leiden","Rotterdam","The Hague","Haarlem"},
             {"Stockholm","Arboga","Arvika","Sigtuna","Uppsala"}};
+    String [][]info={{"1200000","295km^2","4390000","696km^2","397000","550km^2","570000","123","475000","-----"},
+            {"3300000","527k^2","310400","---__----","299000","192km^2","324000","1111","315200","50km^2"},
+            {"5000000","173","3340000","254","950000","750","298000","idk","208000","idk2"},
+            {"8000000","1170km^2","2280000","499","2550000","3080","1495000","1024","873000","idk3"},
+            {"3350000","220","1200000","320","947000","idk4","928000","idk5","632000","370"},
+            {"2300000","614","639000","1010","564000","idk6","419000","idk7","360000","idk8"},
+            {"9500000","3085","5200000","2679","8800000","1579","744000","250","506000","416"},
+            {"13920000","2200","2660000","223","1475000","827","738000","390","720000","328"},
+            {"12400000","603","16780000","1500","8440000","709","7000000","426","4500000","206"},
+            {"21500000","16410","15621000","12000","5300000","13590","859000","2063","8000000","11434"},
+            {}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
